50303c2e99fffdcfba18229dbb5c07a72e36fdbb
JAPH254 <omanyujapheth@gmail.com>
2025-08-26 16:52:17 +0300

    fix: adjust layout spacing in Home component for improved UI

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="max-w-6xl mx-auto p-4">
+      <div className="mx-auto p-4">
         {status === "loading" && (
           <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
             {Array.from({ length: 8 }).map((_, i) => (
@@ -29,7 +29,7 @@ export default function Home() {
 
         {list && (
           <>
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+            <div className="grid grid-cols-2 md:grid-cols-4  gap-4 mt-24">
               {list.results.map((m) => (
                 <MovieCard key={m.id} m={m} />
               ))}
