20b8127765d7795a4d736612503763876096e57f
JAPH254 <omanyujapheth@gmail.com>
2025-08-26 15:27:48 +0300

    feat: configure CORS settings and allowed hosts in Django settings

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-fe(*2p8b&a00wky(hs)fd1+ta3ehp_7#a2d9q%8xz!enp=8v8$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173'
+]
+
 
 
 # Application definition
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
@@ -47,6 +53,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
