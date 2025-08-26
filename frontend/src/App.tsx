88c4f6713d3a963291b9d8aa0e1f0f68b1394a6e
JAPH254 <omanyujapheth@gmail.com>
2025-08-26 15:27:30 +0300

    feat: implement routing in App component with Home as default route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
