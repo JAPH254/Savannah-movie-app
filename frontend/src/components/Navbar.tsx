b0685cc2f5432ff2591ef1ebd02ec9c8cc48a8a9
JAPH254 <omanyujapheth@gmail.com>
2025-08-26 16:58:40 +0300

    fix: update Navbar header class for consistent positioning and text color

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   const q = useSelector((s) => s.movies.q);
 
   return (
-    <header className="sticky top-0 bg-white shadow z-10">
+    <header className="fixed top-0 bg-white w-full  shadow z-10">
       <div className="max-w-6xl mx-auto flex items-center justify-between px-4 py-3">
         <h1 className="text-2xl font-bold text-gray-800">🎥 Movie Explorer</h1>
         <input
@@ -14,7 +14,7 @@ export default function Navbar() {
           placeholder="Search movies…"
           value={q}
           onChange={(e) => d(setQuery(e.target.value))}
-          className="border rounded-lg px-3 py-2 w-64 focus:ring-2 focus:ring-blue-500 outline-none"
+          className="border text-black rounded-lg px-3 py-2 w-64 focus:ring-2 focus:ring-blue-500 outline-none"
         />
       </div>
     </header>
