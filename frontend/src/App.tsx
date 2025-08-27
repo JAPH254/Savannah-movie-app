f35ed70539c94e2d9c6dffe2e8caf725d997459f
JAPH254 <omanyujapheth@gmail.com>
2025-08-27 17:33:29 +0300

    feat: add MovieDetails route to display movie information

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,33 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import MovieDetails from "./components/MovieDetails";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route
+        path="/movie/:id"
+        element={
+          <MovieDetails
+            m={{
+              id: 0,
+              title: "",
+              original_title: "",
+              overview: "",
+              poster_path: null,
+              backdrop_path: null,
+              release_date: "",
+              vote_average: 0,
+              vote_count: 0,
+              popularity: 0,
+              genre_ids: [],
+              original_language: "",
+              media_type: "",
+            }}
+          />
+        }
+      />
     </Routes>
   );
 }
