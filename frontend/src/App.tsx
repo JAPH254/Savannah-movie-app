36f931044ea1a951a4a340f9781df7a1456ce4f6
JAPH254 <omanyujapheth@gmail.com>
2025-08-27 19:41:40 +0300

    refactor: simplify MovieDetails route by removing inline props

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,28 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route
-        path="/movie/:id"
-        element={
-          <MovieDetails
-            m={{
-              id: 0,
-              title: "",
-              original_title: "",
-              overview: "",
-              poster_path: null,
-              backdrop_path: null,
-              release_date: "",
-              vote_average: 0,
-              vote_count: 0,
-              popularity: 0,
-              genre_ids: [],
-              original_language: "",
-              media_type: "",
-            }}
-          />
-        }
-      />
+      <Route path="/movie/:id" element={<MovieDetails />} />
     </Routes>
   );
 }
