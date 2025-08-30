9256fe10cf5974b4a5ff3c9eeb4281850bf757c3
JAPH254 <omanyujapheth@gmail.com>
2025-08-30 10:39:29 +0300

    feat: persisted the user

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loginUser, selectAuth } from "../slices/authSlice";
 import { AppDispatch } from "../store";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
+  const navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
-  const { status, error } = useSelector(selectAuth);
+  const { status, error, token } = useSelector(selectAuth);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    if (status === "succeeded" && token) {
+      navigate("/");
+    }
+  }, [status, token, navigate]);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
