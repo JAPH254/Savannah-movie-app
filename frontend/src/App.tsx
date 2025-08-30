26af89a7a3770b80e30bb8fda57da37b8fce4068
JAPH254 <omanyujapheth@gmail.com>
2025-08-30 10:40:12 +0300

    feat: fully protected the routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,30 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import MovieDetails from "./components/MovieDetails";
+import ProtectedRoute from "./components/ProtectdRoutes";
 import LoginPage from "./components/LoginPage";
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/movie/:id" element={<MovieDetails />} />
-      <Route path="/login" element={<LoginPage/>} />
+      <Route path="/login" element={<LoginPage />} />
+
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/movie/:id"
+        element={
+          <ProtectedRoute>
+            <MovieDetails />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
