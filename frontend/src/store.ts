ef3f57cd63c8bcf63be269a91fbdd1ded2fb9744
JAPH254 <omanyujapheth@gmail.com>
2025-08-30 09:42:47 +0300

    feat: add auth reducer to the store configuration

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import moviesReducer from "./slices/MovieSlice";
-
+import authReducer from "./slices/authSlice";
 export const store = configureStore({
   reducer: {
     movies: moviesReducer,
+    auth: authReducer,
   },
 });
 
