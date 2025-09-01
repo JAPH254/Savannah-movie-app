d45cba2050da692f9a3878d6b2146512c7ebcf39
JAPH254 <omanyujapheth@gmail.com>
2025-09-01 16:11:16 +0300

    feat: removed the deubugging code

--- frontend/src/services/movies.ts
+++ frontend/src/services/movies.ts
@@ -3,7 +3,6 @@ import { MoviePage, Movie } from "../types/movie";
 
 export async function getTrending(page = 1): Promise<MoviePage> {
   const { data } = await api.get<MoviePage>("/api/trending/", { params: { page } });
-  console.log(data)
   return data;
 }
 
